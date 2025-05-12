be155e747363fb48a0f8375e512299f949b30534
Mpho Tshawe Maudu <s221493107@mandela.ac.za>
2025-05-12 09:09:54 +0200

    Add project files.

---
+++ TMTTRUCKS/Controllers/Collection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TMTTRUCKS.Controllers
+{
+    public class Collection : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TMTTRUCKS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TMTTRUCKS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
